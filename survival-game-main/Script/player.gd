2c0db10472db1d428e97cb4b60cdeb805a8e5d65
Dasher12345 <davidasher2003@gmail.com>
2025-05-09 16:47:41 +0500

    Commit 2: "[Video #1] Completed feature"
    - Added walking animation to the player.
    - Added a new scene for the world.
    - Added a new scene for the camera.

--- survival-game-main/Script/player.gd
+++ survival-game-main/Script/player.gd
@@ -16,4 +16,35 @@ func _physics_process(delta: float) -> void:
 	
 	
 	velocity = direction * speed
-	move_and_slide()
+	move_and_slide()    
+	
+	play_anim(direction)
+
+
+func play_anim(direction):
+	if player_state == "Idle":
+		$AnimatedSprite2D.play("Idle")
+	if player_state == "walking":
+		if direction.y == -1:
+			$AnimatedSprite2D.play("U-walk")
+		if direction.y == 1:
+			$AnimatedSprite2D.play("D-walk")
+		if direction.x == 1:
+			$AnimatedSprite2D.play("R-walk")
+		if direction.x == -1:
+			$AnimatedSprite2D.play("L-walk")
+		
+		
+		if direction.x > 0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("DR-walk")
+		if direction.x > 0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("UR-walk")
+		if direction.x < -0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("DL-walk")
+		if direction.x < -0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("UL-walk")
+
+
+
+func player():
+	pass
