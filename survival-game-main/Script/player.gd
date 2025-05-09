5e409a5251261d9317e9b8f9cbc91933145eff43
Dasher12345 <davidasher2003@gmail.com>
2025-05-09 15:19:05 +0500

    Commit 1:"[Video #1] Base setup & partial feature" - Added charecter movement (no animation yet) - Added the animation sprite sheet for movement - Added the animation sprite sheet for attack - Added the animation sprite sheet for idle - Added the animation sprite sheet for death

---
+++ survival-game-main/Script/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+var speed = 100
+
+var player_state
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "Idle"
+	elif direction.x != 0 or direction.y != 0:
+		player_state = "walking"
+	
+	
+	velocity = direction * speed
+	move_and_slide()
