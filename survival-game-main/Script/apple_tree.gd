f7ef8eb421671c9251b6f3e4d7e452fd8b6dc363
Dasher12345 <davidasher2003@gmail.com>
2025-05-13 20:13:57 +0500

    Commit 1: [Video #3] Base setup & partial feature

---
+++ survival-game-main/Script/apple_tree.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var state = "no apples"
+var player_in_area = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if state == "no apples":
+		$growth_timer.start()
+
+
+func _process(delta: float) -> void:
+	if state == "no apples":
+		$AnimatedSprite2D.play("no apples")
+	if state == "apples":
+		$AnimatedSprite2D.play("apples")
+		if player_in_area:
+			if Input.is_action_just_pressed("Interact"):
+				state = "no apples"
+				$growth_timer.start()
+
+func _on_pickable_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+
+
+func _on_pickable_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false
+
+func _on_growth_timer_timeout() -> void:
+	if state == "no apples":
+		state = "apples"
