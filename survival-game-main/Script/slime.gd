7b3c747c723016a2ac135270e0ccbea283b36734
Dasher12345 <davidasher2003@gmail.com>
2025-05-18 03:48:06 +0500

    Commit 2: "[Video #8] Completed feature"

--- survival-game-main/Script/slime.gd
+++ survival-game-main/Script/slime.gd
@@ -8,6 +8,9 @@ var dead = false
 var player_in_area = false
 var player
 
+@onready var slimeball = $slime_collectable
+@export var slimeitem: InvItem
+
 
 func _ready() -> void:
 	dead = false
@@ -56,4 +59,27 @@ func death():
 	dead = true
 	$AnimatedSprite2D.play("death")
 	await get_tree().create_timer(0.7).timeout
+	drop_slime()
+	
+	$AnimatedSprite2D.visible = false
+	$hitbox/CollisionShape2D.disabled = true
+	$detection_area/CollisionShape2D.disabled = true
+
+
+
+func drop_slime():
+	slimeball.visible = true
+	$slime_collect_area/CollisionShape2D.disabled = false
+	smile_collect()
+
+
+
+func smile_collect():
+	await get_tree().create_timer(1.5).timeout
+	slimeball.visible = false
+	player.collect(slimeitem)
 	queue_free()
+
+func _on_slime_collect_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player = body
